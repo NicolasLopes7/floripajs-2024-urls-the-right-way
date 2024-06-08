c64a79b885a70d978b50707de4c4383b1b754a36
Nicolas Lopes <nicolas@clerk.dev>
2024-06-08 13:32:06 -0300

    remove reset

--- src/app/components/data-table-toolbar.tsx
+++ src/app/components/data-table-toolbar.tsx
@@ -1,9 +1,7 @@
 "use client";
 
-import { Cross2Icon } from "@radix-ui/react-icons";
 import { Table } from "@tanstack/react-table";
 
-import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { DataTableViewOptions } from "./data-table-view-options";
 
@@ -36,11 +34,6 @@ export function DataTableToolbar<TData>({
     searchParams.set("search", search);
   };
 
-  const handleReset = () => {
-    table.resetColumnFilters();
-    searchParams.clear();
-  };
-
   return (
     <div className="flex items-center justify-between">
       <div className="flex flex-1 items-center space-x-2">
@@ -63,16 +56,6 @@ export function DataTableToolbar<TData>({
             title="Priority"
             options={priorities}
           />
-        )}
-        {isFiltered && (
-          <Button
-            variant="ghost"
-            onClick={handleReset}
-            className="h-8 px-2 lg:px-3"
-          >
-            Reset
-            <Cross2Icon className="ml-2 h-4 w-4" />
-          </Button>
         )}
       </div>
       <DataTableViewOptions table={table} />
